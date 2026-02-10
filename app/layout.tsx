2c0a7fc68d3611053c75567cf4024812d91c36f8
Harrison Cutrone <harrison@ifixit.com>
2026-02-09 23:50:40 -0700

    head: Fix errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export const metadata: Metadata = {
       siteName: "Denver Bike Fest",
       images: [
          {
-            url: "/logo.png",
+            url: "/logo_vertical.png",
             alt: "Denver Bike Fest Logo",
          },
       ],
@@ -93,19 +93,17 @@ export default async function RootLayout({
    const showSite = headerStore.get("x-show-site") === "true";
    return (
       <html lang="en" className={poppins.className}>
-         <head>
-            <Script id="fundraiseup" strategy="afterInteractive">
-               {`
-						(function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track,openCheckout'
-						.split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
-						:function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
-						j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a+'';
-						t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=5;o.h=w.location.href;o.l=[];
-						for(i=0;i<8;i++)o[l[i]]=o(l[i]);w[n]=o}
-						})(window,document,'script','FundraiseUp','AUAHPMKJ');
-					`}
-            </Script>
-         </head>
+         <Script id="fundraiseup" strategy="afterInteractive">
+            {`
+               (function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track,openCheckout'
+               .split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
+               :function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
+               j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a+'';
+               t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=5;o.h=w.location.href;o.l=[];
+               for(i=0;i<8;i++)o[l[i]]=o(l[i]);w[n]=o}
+               })(window,document,'script','FundraiseUp','AUAHPMKJ');
+            `}
+         </Script>
          <body>
             <Theme
                appearance="light"

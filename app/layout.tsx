da3137c3db4daeb84d8148c317d452d6f73bc267
Harrison Cutrone <harrison@ifixit.com>
2026-02-16 23:11:22 -0700

    Donate: Remove FundraiseUp script
    The fundraise popup does not give the option to select a shirt to pre
    order. Now that we're directly advertising the shirt, we should make
    sure the user can always select the shirt when donating.
    
    Let's link to the standalone donation page instead.

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,25 +94,6 @@ export default async function RootLayout({
 }) {
    return (
       <html lang="en" className={poppins.className}>
-         <Script id="fundraiseup" strategy="afterInteractive">
-            {`
-               (function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track,openCheckout'
-               .split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
-               :function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
-               j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a+'';
-               t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=5;o.h=w.location.href;o.l=[];
-               for(i=0;i<8;i++)o[l[i]]=o(l[i]);w[n]=o}
-               })(window,document,'script','FundraiseUp','AUAHPMKJ');
-
-               (function(){
-                  var observer = new MutationObserver(function(){
-                     var el = document.getElementById('XBWGTSDK');
-                     if(el) el.remove();
-                  });
-                  observer.observe(document.documentElement,{childList:true,subtree:true});
-               })();
-            `}
-         </Script>
          <body>
             <Theme
                appearance="light"
